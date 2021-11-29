d410c4c95a87dbf31d78f2fd08220716a6f34407
cliffhall <cliff@futurescale.com>
2021-11-28 19:17:19 -0500

    Update IFismo interface id

--- scripts/constants/supported-interfaces.js
+++ scripts/constants/supported-interfaces.js
@@ -4,6 +4,6 @@
  * @author Cliff Hall <cliff@futurescale.com> (https://twitter.com/seaofarrows)
  */
 exports.InterfaceIds = {
-    IFismo:              "0x2721f330",
+    IFismo:              "0xba1d128e",
     IERC165:             "0x01ffc9a7",
 };
