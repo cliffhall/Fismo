89a49d618690713159d818c7a4296f52e70e64cd
cliffhall <cliff@futurescale.com>
2022-06-25 13:52:03 -0400

    Redeploy and update SDK

--- scripts/domain/util/deployments.js
+++ scripts/domain/util/deployments.js
@@ -12,18 +12,6 @@ exports.Deployments = {
         "Operator": ""
     },
 
-    // Rinkeby Ethereum Testnet
-    "rinkeby": {
-        "Fismo": "0x29f74438e3bd01bfee8b5dEa04cDf37f29C68fcF",
-        "Operator": "0x2eaa6Aa5D3585CD0dA03F0201B5CEbeD19DbF71D"
-    },
-
-    // Ropsten Ethereum Testnet
-    "ropsten": {
-        "Fismo": "",
-        "Operator": ""
-    },
-
     // Polygon Matic Mainnet
     "matic": {
         "Fismo": "",
@@ -32,8 +20,8 @@ exports.Deployments = {
 
     // Polygon Mumbai Testnet
     "mumbai": {
-        "Fismo": "0xaBF03a7c6fd22d5661aFb27940AC3f7C74e59D30",
-        "Operator": "0x4158D41Cc1E4C1962E0f9FEbEDf85679989D351A"
+        "Fismo": "0x6b9360daA123B6ed97e4dD07E23BAE7e73325a25",
+        "Operator": "0x9A95a33f0E7f810F6d7597992524D0Ead57f5ed4"
     }
 
 };
